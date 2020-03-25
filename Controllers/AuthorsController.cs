de8b95c22ad41b81bd5ba17ffb3a99c92c545bef
Henry Su <henrysu2@icloud.com>
2020-03-25 00:34:02 -0400

    new changes

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -1,8 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using Fisher.Bookstore.Services;
+using Fisher.Bookstore.Models;
+
 namespace Fisher.Bookstore.Controllers
 {
+   
 
-    public class AuthorsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthorsController : ControllerBase
     {
+        private IAuthorsRepository authorsRespository;
+        public AuthorsController(IAuthorsRepository repository){
+            authorsRespository = repository;
+        }
+        [HttpGet]
+        public IActionResult GetAll(){
+            return Ok(authorsRespository.GetAuthors());
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult Get(int authorId){
+            if(!authorsRespository.AuthorExists(authorId)){
+                return NotFound();
+            }
+            return Ok(authorsRespository.GetAuthor(authorId));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Author author ){
+            var authorId = authorsRespository.AddAuthor(author);
+            return Created($"https://localhost:5001/api/authors/{authorId}", author);
+        }
+
+        [HttpPut("{authorId}")]
+        public IActionResult Put(int authorId, [FromBody] Author author){
+            if(authorId != author.Id){
+                return BadRequest();
+            }
+
+            if(!authorsRespository.AuthorExists(authorId)){
+                return NotFound();
+            }
+            authorsRespository.UpdateAuthor(author);
+            return Ok(author);
+        }
+
+        [HttpDelete("{authorId}")]
+        public IActionResult Delete(int authorId){
+
+            if(!authorsRespository.AuthorExists(authorId)){
+                return NotFound();
+            }
+
+            authorsRespository.DeleteAuthor(authorId);
+            return Ok();
+
+        }
 
     }
 }
