b36626ddedf7bb90cc055ea8a78f0dcaa16da71a
Henry Su <henrysu2@icloud.com>
2020-04-29 22:10:14 -0400

    new changes

--- Services/AuthorRepository.cs
+++ Services/AuthorRepository.cs
@@ -1,8 +1,55 @@
+using System.Collections.Generic;
+using Fisher.Bookstore.Data;
+using Fisher.Bookstore.Models;
+
 namespace Fisher.Bookstore.Services
 {
 
-    public class AuthorsRepository
+    public class AuthorssRepository : IAuthorRepository
     {
+        private readonly BookstoreContext db;
 
+        public AuthorRepository(BookstoreContext db)
+        {
+            this.db = db;
+        }
+
+        public int AddAuthor(Author author)
+        {
+            db.Authors.Add(author);
+            db.SaveChanges();
+            return author.Id;
+        }
+
+        public bool Authorexists(int authorId)
+        {
+            return (db.Authors.Find(authorId) != null);
+
+        }
+
+        public void DeleteAuthor(int authorId)
+        {
+            var author = db.Authors.Find(authorId);
+            db.Authors.Remove(author);
+            db.SaveChanges();
+        }
+
+        public Book GetAuthor(int authorId)
+        {
+            return db.Authors.Find(authorId);
+        }
+
+        public IEnumerable<Author> GetAuthors()
+        {
+            return db.Authors;
+        }
+
+        public void UpdateAuthor(Author author)
+        {
+            var updateAuthor = db.Authors.Find(author.Id);
+            updateAuthor.Name = author.Name;
+            db.Authors.Update(updateAuthor);
+            db.SaveChanges();
+        }
     }
 }
